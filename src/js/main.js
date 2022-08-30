531e874823274a1005650f7bc855e7152f5a1588
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-08-30 16:21:27 +0300

    end

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,10 @@ import { cardsTemplate } from './markup-template';
 import LoadMoreBtn from './load-more-btn';
 import { refs } from './refs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // import throttle from 'lodash.throttle';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 const newsApiService = new NewsApiService();
 const loadMoreBtn = new LoadMoreBtn({
@@ -15,6 +16,12 @@ const loadMoreBtn = new LoadMoreBtn({
 
 refs.searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 function onSearch(e) {
   e.preventDefault();
@@ -32,7 +39,6 @@ function onSearch(e) {
   newsApiService.resetPage();
   clearArticlesContainer();
   fetchArticles();
-  
 }
 
 function fetchArticles() {
@@ -51,6 +57,7 @@ function fetchArticles() {
 
     if (articles.length > 0) {
       Notify.success(`Hooray! We found ${articles.length} images.`);
+      lightbox.refresh();
     }
   });
 }
